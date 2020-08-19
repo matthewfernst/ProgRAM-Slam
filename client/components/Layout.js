36947b1cd52993a7be0e01dd90095a8df8062f0c
Max <maxrosoff1@gmail.com>
2020-08-19 16:01:28 -0600

    Part 2

--- client/components/Layout.js
+++ client/components/Layout.js
@@ -1,12 +1,9 @@
 import React from "react";
 
 import { Button, Container, Grid, Typography } from "@material-ui/core";
-import {sendServerRequest} from "../utils/api";
 
 const Layout = () =>
 {
-	let request = {requestType: "postRequest", payload: "Test"};
-
 	return (
 		<Container>
 			<Grid
@@ -20,9 +17,6 @@ const Layout = () =>
 			>
 				<Grid item>
 					<Typography variant={"h2"}>Hello World!</Typography>
-				</Grid>
-				<Grid item>
-					<Button color={"primary"} onClick={() => sendServerRequest(request)}>Click Me To Hit API</Button>
 				</Grid>
 			</Grid>
 		</Container>
