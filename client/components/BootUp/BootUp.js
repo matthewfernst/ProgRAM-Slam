049886a89e7fe933671719d06a8adc9086ac511e
Max <maxrosoff1@gmail.com>
2020-08-19 21:39:17 -0600

    Kill Monster Main

--- client/components/BootUp/BootUp.js
+++ client/components/BootUp/BootUp.js
@@ -30,84 +30,16 @@ const BootUp = props =>
 			<Box p={4}>
 				<Grid container direction={"column"} spacing={4}>
 					<Grid item>
-						<Grid container spacing={4}>
-							<Grid item>
-								<img src={CSULogo} style={{width: 100, height: 100}}/>
-							</Grid>
-							<Grid item>
-								<Grid container justify={"center"} alignContent={"center"} alignItems={"center"} direction={"column"} style={{height: '100%'}}>
-									<Grid>
-										Cam Open Source OS
-									</Grid>
-									<Grid>
-										Test
-									</Grid>
-								</Grid>
-							</Grid>
-							<Grid item>
-								<Grid container justify={"center"} direction={"column"} style={{height: '100%'}}>
-									<Grid>
-										Cam Open Source OS
-									</Grid>
-									<Grid>
-										Test
-									</Grid>
-								</Grid>
-							</Grid>
-						</Grid>
+						<HeaderAndReleaseData />
 					</Grid>
 					<Grid item>
 						Text
 					</Grid>
 					<Grid item>
-						<Grid container direction={"column"} style={{paddingLeft: 20}}>
-							<Grid item>
-								<Box pl={1} pr={1}>
-									<Grid container spacing={2}>
-										<Grid item>
-											User:
-										</Grid>
-										<Grid item>
-											Cam The Ram
-										</Grid>
-									</Grid>
-								</Box>
-							</Grid>
-							<Grid item>
-								<Box pl={1} pr={1}>
-									<Grid container spacing={2}>
-										<Grid item>
-											Device Name:
-										</Grid>
-										<Grid item>
-											Event Name Here
-										</Grid>
-									</Grid>
-								</Box>
-							</Grid>
-							<Grid item>
-								<Box pl={1} pr={1}>
-									<Grid container spacing={2}>
-										<Grid item>
-											Memory Test:
-										</Grid>
-										<Grid item>
-											20Kb
-										</Grid>
-									</Grid>
-								</Box>
-							</Grid>
-						</Grid>
+						<SystemInfo />
 					</Grid>
 					<Grid item>
-						<Grid container direction={"column"} spacing={1}>
-							<Grid item>
-								Detecting Primary Drive (C) . . . . . . SUCCESS
-							</Grid>
-							<Grid item>
-								Detecting Secondary Drive (D) . . . . . . SUCCESS
-							</Grid>
-						</Grid>
+						<DriveInfo />
 					</Grid>
 					<Grid item>
 						Initializing OS . . . . . . SUCCESS
@@ -117,14 +49,97 @@ const BootUp = props =>
 					</Grid>
 				</Grid>
 			</Box>
-			{startup ?
-				<ReactAudioPlayer
-					src={AppleStartUp}
-					autoPlay = {true}
-				/>
-				: null }
+			{startup ? <ReactAudioPlayer src={AppleStartUp} autoPlay={true} controls={false}/> : null}
 		</>
 	);
 };
 
+const HeaderAndReleaseData = props =>
+{
+	return(
+		<Grid container spacing={4}>
+			<Grid item>
+				<img src={CSULogo} style={{width: 100, height: 100}}/>
+			</Grid>
+			<Grid item>
+				<Grid container justify={"center"} alignContent={"center"} alignItems={"center"} direction={"column"} style={{height: '100%'}}>
+					<Grid>
+						Cam Open Source OS
+					</Grid>
+					<Grid>
+						Test
+					</Grid>
+				</Grid>
+			</Grid>
+			<Grid item>
+				<Grid container justify={"center"} direction={"column"} style={{height: '100%'}}>
+					<Grid>
+						Cam Open Source OS
+					</Grid>
+					<Grid>
+						Test
+					</Grid>
+				</Grid>
+			</Grid>
+		</Grid>
+	);
+}
+
+const SystemInfo = props =>
+{
+	return(
+		<Grid container direction={"column"} style={{paddingLeft: 20}}>
+			<Grid item>
+				<Box pl={1} pr={1}>
+					<Grid container spacing={2}>
+						<Grid item>
+							User:
+						</Grid>
+						<Grid item>
+							Cam The Ram
+						</Grid>
+					</Grid>
+				</Box>
+			</Grid>
+			<Grid item>
+				<Box pl={1} pr={1}>
+					<Grid container spacing={2}>
+						<Grid item>
+							Device Name:
+						</Grid>
+						<Grid item>
+							Event Name Here
+						</Grid>
+					</Grid>
+				</Box>
+			</Grid>
+			<Grid item>
+				<Box pl={1} pr={1}>
+					<Grid container spacing={2}>
+						<Grid item>
+							Memory Test:
+						</Grid>
+						<Grid item>
+							20Kb
+						</Grid>
+					</Grid>
+				</Box>
+			</Grid>
+		</Grid>
+	);
+}
+
+const DriveInfo = props =>
+{
+	return(
+		<Grid container direction={"column"} spacing={1}>
+			<Grid item>
+				Detecting Primary Drive (C) . . . . . . SUCCESS
+			</Grid>
+			<Grid item>
+				Detecting Secondary Drive (D) . . . . . . SUCCESS
+			</Grid>
+		</Grid>
+	);
+}
 export default BootUp;
