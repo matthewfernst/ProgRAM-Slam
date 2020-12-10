45a70ead48f9039b0acb051cd2ae93644c302290
Max Rosoff <me@maxrosoff.com>
2020-12-10 12:00:12 -0700

    Clean Up Repo Files

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ const outputDirectory = "dist";
 module.exports = {
 	entry: ["@babel/polyfill", "./client/index.js"],
 	devServer: { port: 3000, open: true, hot: true },
-	devtool: 'cheap-source-map',
+	devtool: 'eval-source-map',
 	module: {
 		rules: [
 			{test: /\.jsx?$/, exclude: /node_modules/, loader: 'babel-loader', options: { presets: ['@babel/preset-env', '@babel/react'] }},
@@ -16,17 +16,10 @@ module.exports = {
 			{
 				test: /\.s[ac]ss$/i, use:
 					[
-						{ loader: 'style-loader' },
-						{ loader: 'css-loader' },
-						{ loader: 'postcss-loader', options:
-								{
-									plugins: function()
-									{
-										return [require('precss'), require('autoprefixer')];
-									}
-								}
-						},
-						{ loader: 'sass-loader' }
+						{loader: 'style-loader'},
+						{loader: 'css-loader'},
+						{loader: 'postcss-loader', options: {postcssOptions: {plugins: ['autoprefixer', 'precss']}}},
+						{loader: 'sass-loader'}
 					]
 			},
 			{test: /\.(woff|woff2|eot|ttf|otf|png|svg|jpe?g|gif|mp4|wav|mp3)$/i, loader: ['file-loader']}
